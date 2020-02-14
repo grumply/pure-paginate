a292c23de7eb627f3cf038525d595a2179802317
Sean Hickman <sean@grumply.com>
2020-02-13 23:33:18 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure, stdenv }:
 mkDerivation {
   pname = "pure-paginate";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure ];
   homepage = "github.com/grumply/pure-paginate";
